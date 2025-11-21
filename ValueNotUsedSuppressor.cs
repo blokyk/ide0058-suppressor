83dfd07c373bc7d0b9552f7f1be2586fa0da8c0d
blokyk <courvoisier.zoe@gmail.com>
2025-11-21 23:08:19 +0000

    refact: move code around for clarity
    god i gotta do something about the exception list code, it's kinda
    disgusting

--- ValueNotUsedSuppressor.cs
+++ ValueNotUsedSuppressor.cs
@@ -19,9 +19,49 @@ public class ValueNotUsedSuppressor : DiagnosticSuppressor
         "System.Text.StringBuilder"
     };
 
+    private int _currNamesListHash = 0;
+
+    public override void ReportSuppressions(SuppressionAnalysisContext context) {
+        foreach (var diag in context.ReportedDiagnostics) {
+            var tree = diag.Location.SourceTree!;
+            var model = context.GetSemanticModel(tree);
+
+            // computing the list of exceptions is mildly expensive, but thankfully most
+            // of nodes will probably share the same editorconfig so we can just cache it,
+            // but we still need to check (and update if necessary) just to be correct
+            UpdateExceptionListFrom(context.Options.AnalyzerConfigOptionsProvider.GetOptions(tree));
+
+            if (ShouldSuppress(diag, tree, model))
+                context.ReportSuppression(Suppression.Create(_suppressIDE0058Descriptor, diag));
+        }
+    }
+
+    bool ShouldSuppress(Diagnostic diag, SyntaxTree tree, SemanticModel model) {
+        // we want to suppress symbols that, are fluent interfaces, i.e. that are:
+        //      1. are methods
+        //      2. comes from a fluent type
+        //      3. returns a fluent type
+
+        var node = tree.GetRoot().FindNode(diag.Location.SourceSpan, getInnermostNodeForTie: true);
+        var nodeSymbolInfo = model.GetSymbolInfo(node).Symbol;
+
+        // if the node isn't typed, just give up
+        if (nodeSymbolInfo is null)
+            return false;
+
+        // if it's not a method we don't care
+        if (nodeSymbolInfo is not IMethodSymbol methodInfo)
+            return false;
+
+        var containingTypeName = GetFullMetadataName(methodInfo.ContainingType);
+
+        return _exceptionsMetadataNames.Contains(containingTypeName);
+
+    }
+
     private HashSet<string> _exceptionsMetadataNames = _builtinExceptions;
     private void SetNewExceptionList(string[] names) {
-        _exceptionsMetadataNames = new();
+        _exceptionsMetadataNames = [];
 
         foreach (var name in names) {
             var trimmedName = name.Trim();
@@ -31,61 +71,19 @@ public class ValueNotUsedSuppressor : DiagnosticSuppressor
         }
     }
 
-    private int _currNamesListHash = 0;
-
-    private const string _supLogPath = "/home/blokyk/csharp/suppressor.log";
-
-    [Conditional("LOG")]
-    private static void Log(string s) => File.AppendAllText(_supLogPath, DateTime.Now + " > " + s + "\n");
-
-    public override void ReportSuppressions(SuppressionAnalysisContext context) {
-        Log("ReportSuppressions() called...");
-
-        foreach (var diag in context.ReportedDiagnostics) {
-            var tree = diag.Location.SourceTree!;
-
-            var opts = context.Options.AnalyzerConfigOptionsProvider.GetOptions(tree);
-
-            if (opts.TryGetValue("dotnet_fluent_types", out var names)) {
-                Log("dotnet_fluent_types = " + names);
-                if (names.GetHashCode() != _currNamesListHash) {
-                    _currNamesListHash = names.GetHashCode();
-                    SetNewExceptionList(names.Split(','));
-                }
-            } else {
-                _exceptionsMetadataNames = _builtinExceptions;
-                Log("dotnet_fluent_types doesn't exist!");
-                Log(string.Join(", ", opts.Keys));
+    void UpdateExceptionListFrom(AnalyzerConfigOptions opts) {
+        if (opts.TryGetValue("dotnet_fluent_types", out var names)) {
+            if (names.GetHashCode() != _currNamesListHash) {
+                _currNamesListHash = names.GetHashCode();
+                SetNewExceptionList(names.Split(','));
             }
-
-            Log("fluent_types = [" + string.Join(", ", _exceptionsMetadataNames) + "]");
-
-            var node = tree.GetRoot().FindNode(diag.Location.SourceSpan, getInnermostNodeForTie: true);
-            var model = context.GetSemanticModel(tree);
-
-            var nodeSymbolInfo = model.GetSymbolInfo(node).Symbol;
-
-            if (nodeSymbolInfo is null)
-                continue;
-
-            if (nodeSymbolInfo is not IMethodSymbol methodInfo)
-                continue;
-
-            var containingTypeName = GetFullMetadataName(methodInfo.ContainingType);
-
-            Log("method's containing type is named '" + containingTypeName + "'");
-
-            if (!_exceptionsMetadataNames.Contains(containingTypeName))
-                continue;
-
-            Log("type '" + containingTypeName + "' is fluent, suppressing diagnostic...");
-
-            context.ReportSuppression(Suppression.Create(_suppressIDE0058Descriptor, diag));
+        } else {
+            _exceptionsMetadataNames = _builtinExceptions;
         }
     }
 
     // from https://stackoverflow.com/questions/27105909/get-fully-qualified-metadata-name-in-roslyn
-    public static string GetFullMetadataName(ISymbol s) {
+    internal static string GetFullMetadataName(ISymbol s) {
         if (IsRootNamespace(s)) {
             return string.Empty;
         }
